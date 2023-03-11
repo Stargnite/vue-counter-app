ff3990578cd7891896962598f3fdc93c177aafc3
Sobayo <sobayotoby@gmail.com>
2023-03-11 10:54:41 +0100

    improved error route

--- src/views/ErrorPage.vue
+++ src/views/ErrorPage.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="error-page">
-    <h2>Sorry this page does not exist</h2>
+    <h2>Sorry, it seems the page you're looking for does not exist</h2>
     <router-link to="/">
       <button>Return home</button>
     </router-link>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.error-page {
+  width: 100%;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+h2 {
+  color: rgb(233, 236, 235);
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  padding: 3%;
+}
+</style>
